91b000a2a0ae4bc4191c3ca37f3956288a6d4c83
ninaroscelf <ninaf@adaptivetelehealth.com>
2020-09-15 19:44:35 +0800

    09052020

--- ATH-Resources/Variables/ATHScheduling_Appointment_By_Client.py
+++ ATH-Resources/Variables/ATHScheduling_Appointment_By_Client.py
@@ -6,8 +6,9 @@ TimeCalendar="04:00 - 05:00 PM"
 Time2="12:00PM - 01:00PM"
 Time5="05:00PM - 06:00PM"
 Time6="06:00PM - 07:00PM"
-ApptType1="Online"
+ApptType1="Online - Video"
 ApptType2="In-Person"
+LiveApptType2="In Person"
 Message="Automation Schedule Online Meeting"
 
 
--- ATH-Resources/Variables/ATHScheduling_SetOnlineAppointment_InPersonAppointment_By_Client.py
+++ ATH-Resources/Variables/ATHScheduling_SetOnlineAppointment_InPersonAppointment_By_Client.py
@@ -3,8 +3,9 @@ TimeDisplay="05:00 PM - 06:00 PM"
 TimeCalendar="04:00 - 05:00 PM"
 Time2="12:00PM - 01:00PM"
 TimeDisplay2="12:00 PM - 01:00 PM"
-ApptType1="Online"
+ApptType1="Online - Video"
 ApptType2="In-Person"
+LiveApptType2="In Person"
 Message="Automation Schedule Online Meeting"
 
 
